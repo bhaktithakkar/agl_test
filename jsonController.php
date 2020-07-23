9981154e7055b827476c577a1f272cac86061ac9
Bhakti Thakkar <bhaktikariya@gmail.com>
2020-07-24 01:16:08 +1000

    Creating getJSONData function
    getJSONData function connects with the model to ask model for the data and pass it to the view

--- jsonController.php
+++ jsonController.php
@@ -3,4 +3,9 @@ include_once 'jsonModel.php';
 
 class jsonController{
 
+	public function getJSONData(){
+		$jsonObj = new jsonModel();
+		$catsList = $jsonObj->readJSONData();
+		return $catsList;
+	}
 }
