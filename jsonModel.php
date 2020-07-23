d76ceea214702b6cf1c1ecab087f2376f35d7e51
Bhakti Thakkar <bhaktikariya@gmail.com>
2020-07-24 01:11:05 +1000

    Creating readJSONData function
    readJSONData function reads web service data and loops through the json array

--- jsonModel.php
+++ jsonModel.php
@@ -1,6 +1,28 @@
 <?php
 class jsonModel{
- 
-  public function readJSONData(){
-  }
+	
+	/*
+	* Function to read web service data and get only cats for each person from the json data
+	*/
+	public function readJSONData(){
+  		
+		$jsonData = file_get_contents("http://agl-developer-test.azurewebsites.net/people.json");
+		$data = json_decode($jsonData,true);
+		
+		$catsList = array();
+		
+		//Loop through each person and find their pets
+		foreach($data as $person){
+			//Check if pets array exists for that person
+			if(isset($person['pets'])){
+				foreach($person['pets'] as $pet){
+					//Check if the pet type is Cat then assign to catsList gender associative array
+					if($pet['type'] == 'Cat'){
+						$catsList[$person['gender']][] = $pet['name'];
+					}
+				}
+			}
+		}
+		return $catsList;
+	}
 }
