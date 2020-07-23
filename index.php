57682aaaf16b0d328e50d16e04bfe83363e24d62
Bhakti Thakkar <bhaktikariya@gmail.com>
2020-07-24 01:24:56 +1000

    Creating index.php view file
    View file to display list of cats with Gender of their owner as heading

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+include_once 'jsonController.php';
+$controller = new jsonController();
+$catsList = $controller->getJSONData();
+?>
+<html>
+	<head>
+	<title> AGL Code TEST </title>
+	</head>
+	<body>
+		<div id="catsList">
+		<?php 
+			if(isset($catsList)){
+				foreach($catsList as $key=>$value){
+					asort($value); ?>
+					<h2> <?php echo $key;?></h2>
+					<ul>
+					<?php foeach($value as $catName){?>
+						<li> <?php echo $catName; ?> </li>
+					<?php }  ?>
+					</ul>
+				<?php }
+			}
+		?>
+		<div>
+	</body>
+</html>
